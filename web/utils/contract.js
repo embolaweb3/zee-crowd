d82f4e9ff616fd82b8f987bbf29e02c950f1fcfa
embolaweb3 <163683760+embolaweb3@users.noreply.github.com>
2024-08-28 07:37:56 +0000

    moved the abi

--- web/utils/contract.js
+++ web/utils/contract.js
@@ -1,5 +1,5 @@
 import { ethers } from "ethers";
-import Crowdfunding from "../../contracts/artifacts/contracts/CrowdFunding.sol/Crowdfunding.json";
+import abi from "./abi.json";
 
 const contractAddress = "0x379aC4ffeFf3D91A9F4Ffa55Ba37B73C751Ed63E"; // contract address
 
@@ -7,7 +7,7 @@ export const getContract = () => {
   if (typeof window.ethereum !== "undefined") {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    return new ethers.Contract(contractAddress, Crowdfunding.abi, signer);
+    return new ethers.Contract(contractAddress, abi, signer);
   }
   return null;
 };
